c79cd18d9316fb3e896f8dfae4ac91924b88f8e9
Горбат Глеб Андреевич <proboy4ic@gmail.com>
2024-10-24 12:57:53 +0300

    [task_02] Add lab2 (#41)
    * add lab2
    
    * mark lab2
    
    * add readme
    
    * fix main
    
    * fix main.cpp
    
    * fix main
    
    * fix readme
    
    * fix main
    
    * fix main help me pls
    
    ---------
    
    Co-authored-by: Yana Sitkovets <yana.sitkowecz@yandex.ru>

---
+++ trunk/ii230278/task_02/src/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// Константы для системы
+double alpha = 0.5,
+        beta = 0.6,
+        gamma = 0.6,
+        delta = 0.6;
+
+double gain = 0.8,
+        time_const_0 = 1.1,
+        time_delay = 1.0,
+        time_const = 1.1,
+        desired_value = 20;
+
+// Коэффициенты для расчетов
+double coeff_0 = gain * (1 + (time_delay / time_const_0)),
+        coeff_1 = -gain * (1 + 2 * (time_delay / time_const_0) - (time_const_0 / time_const)),
+        coeff_2 = gain * (time_delay / time_const_0);
+
+// Функция для нелинейного управления
+void nonlinearControl() {
+    const double initial_output = 2;
+    vector<double> outputs;
+    outputs.push_back(initial_output);
+
+    double control_signal = 1.0;
+    vector<double> error_values;
+    error_values.push_back(desired_value - initial_output);
+
+    vector<double> previous_signals;
+    previous_signals.push_back(control_signal);
+
+    // Цикл до достижения желаемого значения
+    while (abs(desired_value - outputs.back()) > 0.01) {
+        double current_error = desired_value - outputs.back();
+        error_values.push_back(current_error);
+
+        control_signal = previous_signals.back() +
+                         coeff_0 * current_error +
+                         coeff_1 * (error_values.size() > 1 ? error_values[error_values.size() - 2] : 0) +
+                         coeff_2 * (error_values.size() > 2 ? error_values[error_values.size() - 3] : 0);
+
+        outputs.push_back(alpha * outputs.back() -
+                          beta * (outputs.size() > 1 ? outputs[outputs.size() - 2] : initial_output) +
+                          gamma * control_signal +
+                          delta * sin(previous_signals.back()));
+
+        previous_signals.push_back(control_signal);
+    }
+
+    // Вывод результатов
+    cout << "Step Output | Error | Control Signal" << endl;
+    for (size_t i = 0; i < outputs.size(); ++i) {
+        cout << i + 1 << " | " << outputs[i] << " | " << (desired_value - outputs[i]) << " | " << previous_signals[i] << endl;
+    }
+}
+
+int main() {
+    nonlinearControl();
+    return 0;
+}
