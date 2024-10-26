bf1342530b603b62f99aad91c3f4ad97fed23979
Zormakrr <zombivlad777@gmail.com>
2024-10-26 13:41:33 +0300

    2labOTIS (#65)

--- readme.md
+++ readme.md
@@ -63,7 +63,7 @@
 |13|Алексей Хитренко||||||||||
 |14|Вирко Егор||||||||||
 |15|Данилюк Тимофей|[ii02602](./trunk/ii02602/)|:white_check_mark:|:white_check_mark:|:white_check_mark:|||||||
-|16|Заруцкий Владислав |[ii02604](./trunk/ii02604/)|:white_check_mark:|:white_check_mark:||||||||
+|16|Заруцкий Владислав |[ii02604](./trunk/ii02604/)|:white_check_mark:|:white_check_mark:|:white_check_mark:|||||||
 |17|Згера Егор|[ii02605](./trunk/ii02605/)|:white_check_mark:|:white_check_mark:|:white_check_mark:||:white_check_mark:||||
 |18|Карпович Ксения|[ii02606](./trunk/ii02606/)|:white_check_mark:|:white_check_mark:||||||||
 |19|Ковальчук Артем|[ii00230308](./trunk/ii00230308/)|:white_check_mark:|:white_check_mark:|:white_check_mark:|||||||
---
+++ trunk/ii02604/task_02/src/Lab2OTIS.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+// Коэффициенты системы
+double koef_1, koef_2, koef_3, koef_4;
+// Параметры системы
+double gain, timekon_0, tm_del, tm_kon, des_out;
+// Параметры регулятора
+double rg_a, rg_b, rg_c;
+
+void nonlinear_system() {
+	// Начальные параметры
+	const short int initial_output = 2;
+	vector<double> output = { initial_output, initial_output };  // Выходные значения
+	double control_signal = 1.0;  // Управляющий сигнал
+	vector<double> error = { des_out - initial_output, des_out - initial_output };  // Ошибка
+	vector<double> previous_control_signal = { control_signal, control_signal };  // Предыдущие значения управляющего сигнала
+
+	// Цикл, пока ошибка не станет меньше заданного порога
+	while (abs(des_out - output.back()) > 0.01)
+	{
+		error.push_back(des_out - output.back());  // Обновление ошибки
+		control_signal = previous_control_signal.back() + rg_a * error.back() + rg_b * error[error.size() - 2] + rg_c * error[error.size() - 3];
+		// Рассчитываем новое значение выхода системы
+		output.push_back(koef_1 * output.back() - koef_2 * output[output.size() - 2] + koef_3 * control_signal + koef_4 * sin(previous_control_signal.back()));
+		previous_control_signal.push_back(control_signal);  // Сохраняем управляющий сигнал
+	}
+
+	// Вывод результатов
+	cout << "Step\tOutput\tError\tControl Signal\n";
+	for (int i = 0; i < output.size(); i++)
+		cout << i + 1 << ";" << output[i] << ";" << error[i] << ";" << previous_control_signal[i] << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	// Ввод параметров системы
+	cout << "Введите коэффициенты системы:\n";
+	cout << "koef_1: "; cin >> koef_1;
+	cout << "koef_2: "; cin >> koef_2;
+	cout << "koef_3: "; cin >> koef_3;
+	cout << "koef_4: "; cin >> koef_4;
+
+	// Ввод параметров системы
+	cout << "Введите параметры системы:\n";
+	cout << "gain: "; cin >> gain;
+	cout << "timekon_0: "; cin >> timekon_0;
+	cout << "tm_del: "; cin >> tm_del;
+	cout << "tm_kon: "; cin >> tm_kon;
+	cout << "des_out (желаемое значение выхода): "; cin >> des_out;
+
+	// Расчет параметров регулятора
+	rg_a = gain * (1 + (tm_del / timekon_0));
+	rg_b = -gain * (1 + 2 * (tm_del / timekon_0) - (timekon_0 / tm_kon));
+	rg_c = gain * (tm_del / timekon_0);
+
+	// Запуск моделирования
+	nonlinear_system();
+
+	return 0;
+}
